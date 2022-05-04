b3202c221bd1dfc316d676cbf55f74e84a399146
duanyhui <3116551464@qq.com>
2022-05-04 17:21:25 +0800

    初次提交

---
+++ src/views/Admin/home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <el-container>
+      <el-main>
+        <h1>admin home</h1>
+        <el-card style="width: 40%">
+
+          <p><i class="el-icon-s-custom" style="margin-right: 18px">Create</i></p>
+        </el-card>
+        <logout></logout>
+      </el-main>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import Logout from "@/components/logout";
+export default {
+  name: "home",
+  components: {Logout},
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Student/home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <el-container>
+      <el-main>
+        <h1>student home</h1>
+        <info-card></info-card>
+        <el-card style="width: 100%; margin-top: 10px">
+          <p><i class="el-icon-s-custom" style="margin-right: 18px">Create</i></p>
+        </el-card>
+        <logout></logout>
+      </el-main>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import Logout from "@/components/logout";
+import InfoCard from "@/components/infoCard";
+export default {
+  name: "home",
+  components: {InfoCard, Logout},
+}
+</script>
+
+<style scoped>
+
+</style>
